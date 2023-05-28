1cb3a0856bbada24a361e82512d7de592a53f4db
Maikel Ordaz <maikelordaz@gmail.com>
2023-05-27 20:48:33 -0400

    message string

--- week4 - Smart Contract Basics/1 - Solidity Syntax/1 - Data Types/4 - String Literals/src/contract.sol
+++ week4 - Smart Contract Basics/1 - Solidity Syntax/1 - Data Types/4 - String Literals/src/contract.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.4;
 
 contract Contract {
-	bytes32 public msg1;
+    bytes32 public msg1;
     string public msg2;
 
     constructor() {
         msg1 = "Hello World";
-        msg2 = "Daniel Marques - Blockchain Developer";
+        msg2 = "Maikel Ordaz - Blockchain Developer";
     }
 }
